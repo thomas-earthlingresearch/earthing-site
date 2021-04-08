5f570b15dcb768c357d4b3e1414b7a885c684ce7
thomas-earthlingresearch <82155485+thomas-earthlingresearch@users.noreply.github.com>
2021-04-08 22:30:11 +1000

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome Earthling!<</h1>
+    <p>Download a sample of our Preview Report below.</p>
+    <p>We hope to hear from you soon!</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Welcome Earthling!</h1>
+    <p>Download a sample of our Preview Report below.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
